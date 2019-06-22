9624e17d83b38b1afcd6e189a60fb5407e13e8fd
Conr86 <cjkneebone@gmail.com>
2019-06-22 17:35:44 +1000

    Fixed test

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from smbus2 import SMBusWrapper
-from sgp30 import Sgp30
+from sgp30 import SGP30
 import time
 with SMBusWrapper(1) as bus:
-    sgp=Sgp30(bus)
+    sgp = SGP30(bus)
     print("resetting all i2c devices")
-    sgp.i2c_geral_call()
+    sgp.i2c_general_call()
     print(sgp.read_features())
     print(sgp.read_serial())
     sgp.init_sgp()
-    print(sgp.read_measurements())
+    for i in range(300):
+        print(sgp.read_measurements())
+        time.sleep(0.1)
+    sgp.store_baseline()
